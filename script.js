f54c7c7a1e0fe2944f306756ae2f8aab6b4f8213
GianniBalti <gianni@baltisolar.be>
2023-03-20 11:49:01 +0100

    updated functionality needs tweaking

--- script.js
+++ script.js
@@ -8,29 +8,79 @@ function Game(title, platform, played) {
     this.played = played;
 }
 
-Game.prototype.getData = function() {
-}
+addBtn.addEventListener("click", addGameToLibrary);
 
-Game.prototype.addGame = function(){}
-
-addBtn.addEventListener("click", () => {
+function addGameToLibrary() {
     const container = document.getElementById("container");
-    console.log(container);
-    let title = document.getElementById("title").value;
-    let platform = document.getElementById("platform").value;
-    let played = document.getElementById("checkbox").checked;
-    const game = new Game(title, platform, played);
-    Games.push(game);  
-    Games.forEach(game => {
-        Games.shift();
-            let card = document.createElement("div");
-            card.className = "game-card-add";
-            card.innerHTML = `<div class="card-title-add">${game.title}</div><div class="card-platform-add">${game.platform}</div><div class="card-played-add">${game.played}</div>`;    
-            container.appendChild(card);
-            console.log(Games)        
-    })
+        container.innerHTML = "";
+        //console.log(container);
+        let title = document.getElementById("title").value;
+        let platform = document.getElementById("platform").value;
+        let played = document.getElementById("checkbox").checked;
+        const game = new Game(title, platform, played);
+        Games.push(game);
+        displayGames();  
+    }
+
+function displayGames() {
+    Games.forEach(function(game, i) {
+        // Games.shift();
+        let card = document.createElement("div");
+        card.id = "card-"+i;
+        card.setAttribute("data-id",i);
+        card.className = "game-card-add";
+        let cardTitle = document.createElement("div");
+        cardTitle.className = "card-title-add";
+        let cardPlatform = document.createElement("div");
+        cardPlatform.className = "card-platform-add";
+        let cardStatus = document.createElement("button");
+        let cardDelete = document.createElement("button");
+        cardStatus.id = "status-"+i;
+        cardTitle.innerText = game.title;
+        cardPlatform.innerText = game.platform;
+        cardDelete.innerText = "Delete";
+        cardDelete.id = "delete-btn"+i;
+        cardDelete.setAttribute("data-id", i);
+        card.append(cardTitle, cardPlatform, cardStatus, cardDelete)
+        cardDelete.addEventListener("click", () => {
+             let currentCard = document.getElementById('card-'+i);
+             console.log(currentCard);
+             currentCard.remove();
+             delete Games[i];
+         });
+        if (game.played == true) {
+            cardStatus.innerText = "Played";
+        } else {
+            cardStatus.innerText = "Not played";
+        }
+        cardStatus.addEventListener("click", () => {
+            let currentStatus = document.getElementById("status-"+i);
+            let currentObject = Games[i];
+            if (currentStatus.innerText === "Played" && currentObject.played === true) {
+                currentObject.played = false;
+                console.log(currentObject.played);
+                console.log(Games);
+                currentStatus.innerText = "Not played";
+            } else {
+                currentObject.played = true;
+                currentStatus.innerText = "Played";
+                console.log(Games);
+            }
+        })
+        container.appendChild(card);
+            
+                 
+    })  
+    }
+
     
 
-    // console.log(Games);
-    
-})
+  
+
+
+
+
+
+
+
+
